edee1824243d6749f86d57e393eb096a5b30547e
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-12 12:54:26 +0600

    creat login from

---
+++ Js/bank.js
@@ -0,0 +1,52 @@
+// // Deposite 
+// document.getElementById('depsite-btn').addEventListener('click', function(){
+    
+//     const depositeField = document.getElementById('deposite-field');
+//     const depositeNewAmountText = depositeField.value;
+//     const depositeNewAmount = parseFloat(depositeNewAmountText);
+
+//     const depositeElement = document.getElementById('diposite-total');
+//     const depositePreviousAmountText = depositeElement.innerText;
+//     const depositePreviousAmount = parseFloat(depositePreviousAmountText)
+//     // depositeElement.innerText = depositeNewAmount;
+
+//     const courrentDepositeAmount = depositePreviousAmount + depositeNewAmount;
+//     depositeElement.innerText = courrentDepositeAmount;
+
+//     const priviousbalancElement = document.getElementById("balance-toatl");
+//     const previousTotalText = priviousbalancElement.innerText;
+//     const previousTotalAmount = parseFloat(previousTotalText)
+
+//     const newTotalBalance = previousTotalAmount + depositeNewAmount;
+//     priviousbalancElement.innerText = newTotalBalance;
+
+//     depositeField.value = '';
+// })
+
+// // withdrow 
+
+// document.getElementById('withdrow-btn').addEventListener('click',function(){
+//     const WithdrowField = document.getElementById('Withdrow-field');
+//     const withdrawText = WithdrowField.value;
+//     const withdeawNewAmount = parseFloat(withdrawText);
+  
+
+//     const WithdrowTotal = document.getElementById('Withdrow-total');
+//     const withpreviousText = WithdrowTotal.innerText;
+//     const withdrawPreviousAmount = parseFloat(withpreviousText)
+
+//     const totalWithdraw = withdrawPreviousAmount + withdeawNewAmount;
+//     WithdrowTotal.innerText = totalWithdraw;
+
+
+//     const balanceToatl = document.getElementById('balance-toatl');
+//     const balanceTotalText = balanceToatl.innerText;
+//     const balanceToatlAmount = parseFloat(balanceTotalText)
+
+//     const newTotalBalance = balanceToatlAmount - withdeawNewAmount;
+//     balanceToatl.innerText = newTotalBalance;
+
+
+//     WithdrowField.value = '';
+    
+// })
---
+++ Js/login.js
@@ -0,0 +1,16 @@
+
+// document.getElementById('submit-btn').addEventListener('click', function(){
+//     const emailField = document.getElementById('email-field');
+//     const email = emailField.value;
+    
+//     const passwordField = document.getElementById('password-field');
+//     const password = passwordField.value;
+    
+//     if(email ==='rhsabbir090@gmail.com' && password ==='rhsabbir008'){
+//         window.location.href ="bank.html";
+//     }
+//     else{
+//        alert('Invalid usuer')
+//     }
+
+// })
