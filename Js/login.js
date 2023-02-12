61e1728788f6ea297c34127a03fcf8d4dade2415
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-12 12:56:32 +0600

    get password

--- Js/login.js
+++ Js/login.js
@@ -3,8 +3,8 @@ document.getElementById('submit-btn').addEventListener('click', function(){
     const emailField = document.getElementById('email-field');
     const email = emailField.value;
     
-//     const passwordField = document.getElementById('password-field');
-//     const password = passwordField.value;
+    const passwordField = document.getElementById('password-field');
+    const password = passwordField.value;
     
 //     if(email ==='rhsabbir090@gmail.com' && password ==='rhsabbir008'){
 //         window.location.href ="bank.html";
